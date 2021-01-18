400ef8dfbb78dcb93aa80f2ffa1f5b01989074e1
Mark Ghaida <markghaida@gmail.com>
2021-01-18 18:42:39 -0500

    commit tree 6a755c8fb9c0d983daa2318a2636c404d46e40f8

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function fetchBooks() {
 
+  // fetch("https://anapioficeandfire.com/api/books")
+  // .then(function(response){
+  //   return response.json();
+  // })
+  // .then(function(books){
+  //   renderBooks(books)
+  // }) 
+
+
+  
+    return fetch("https://anapioficeandfire.com/api/books")
+      .then(resp => resp.json())
+      .then(json => renderBooks(json))
+  
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main')
   books.forEach(book => {
     const h2 = document.createElement('h2')
+    // const h5 = document.createElement('h5')
     h2.innerHTML = book.name
-    main.appendChild(h2)
+    // h5.textContent = `${book.name}'s Total Page Count: ${book.numberOfPages}`
+    main.append(h2)
+ 
+
   })
 }
 
